63b26efe26fd0d597f5b2e51fe162a5ee21f517f
Paul Tagliamonte <paultag@debian.org>
2013-04-30 15:44:42 -0400

    scrape in more from the homepage

--- prototyping/nyc/members.py
+++ prototyping/nyc/members.py
@@ -2,9 +2,45 @@ from util import lxmlize
 
 MEMBER_PAGE = "http://council.nyc.gov/html/members/members.shtml"
 
+
+def scrape_homepage(homepage):
+    ret = {}
+
+    page = lxmlize(homepage)
+
+    ret['image'] = page.xpath(
+        "//td[@class='inside_top_image']//img")[0].attrib['src']
+
+    bio = page.xpath("//td[@class='inside_sub_feature']")[0]
+    ret['bio'] = bio.text_content()
+
+    #infobox = page.xpath("//td[@class='inside_top_text']")[0]
+    #infobits = infobox.xpath(".//strong")
+    #for entry in infobox:
+    #    k, v = entry.text, entry.tail
+    #    if k == "Committees":
+    #        print v
+
+    return ret
+
+
 def scrape_people():
     page = lxmlize(MEMBER_PAGE)
-    print page
+    for entry in page.xpath("//table[@id='members_table']//tr"):
+        entries = entry.xpath(".//td")
+        if entries == []:
+            continue
+
+        name, district, borough, party = entries
+        name = name.xpath(".//a")[0]
+        homepage = name.attrib['href']
+        name, district, borough, party = [x.text for x in
+                                          [name, district, borough, party]]
+
+        info = scrape_homepage(homepage)
+        # person = Person(name, district=district, borough=borough,
+        #                 party=party)
+        print name, district, borough, party
 
 
 if __name__ == "__main__":
