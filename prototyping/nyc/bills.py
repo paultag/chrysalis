2700b2e95cf8d096653917aa8c2936c7e51b8595
Paul Tagliamonte <tag@pault.ag>
2013-05-06 15:06:10 -0400

    start bill work

---
+++ prototyping/nyc/bills.py
@@ -0,0 +1,15 @@
+from util import do_post_back, post_back, lxmlize
+
+
+def scrape_bills():
+    page = lxmlize("http://legistar.council.nyc.gov/Legislation.aspx")
+    form = page.xpath("//form[@name='aspnetForm']")[0]
+    page = do_post_back(form, 'ctl00$ContentPlaceHolder1$btnSwitch', '')
+    form = page.xpath("//form[@name='aspnetForm']")[0]
+    page = post_back(form, **{})  #"ctl00$ContentPlaceHolder1$lstMax": "All"})
+    print page.text_content()
+    print page.xpath("//table[@class='rgMasterTable']")
+
+
+if __name__ == "__main__":
+    scrape_bills()
--- prototyping/nyc/util.py
+++ prototyping/nyc/util.py
@@ -1,9 +1,31 @@
+import urllib
 import urllib2
 import lxml.html
+import time
 
 
 def lxmlize(url):
+    time.sleep(4)
     entry = urllib2.urlopen(url)
     page = lxml.html.fromstring(entry.read())
     page.make_links_absolute(url)
     return page
+
+
+def post_back(form, **kwargs):
+    block = {name: value for name, value in [(obj.name, obj.value)
+                for obj in form.xpath(".//input")]}
+    block.update(kwargs)
+
+    data = urllib.urlencode(block)
+    ret = lxml.html.fromstring(urllib2.urlopen(form.action, data).read())
+
+    ret.make_links_absolute(form.action)
+    return ret
+
+
+def do_post_back(form, event_target, event_argument):
+    block = {}
+    block['__EVENTTARGET'] = event_target
+    block['__EVENTARGUMENT'] = event_argument
+    return post_back(form, **block)
