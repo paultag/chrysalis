ce6254bfefb5717733816e79cd4b1b669d67a8b7
Paul Tagliamonte <tag@pault.ag>
2013-05-13 13:39:40 -0400

    starting to hack up temecula

---
+++ prototyping/temecula/util.py
@@ -0,0 +1,29 @@
+import urllib
+import urllib2
+import lxml.html
+
+
+def lxmlize(url):
+    entry = urllib2.urlopen(url)
+    page = lxml.html.fromstring(entry.read())
+    page.make_links_absolute(url)
+    return page
+
+def post_back(form, **kwargs):
+    block = {name: value for name, value in [(obj.name, obj.value)
+                for obj in form.xpath(".//input")]}
+    block.update(kwargs)
+
+    data = urllib.urlencode(block)
+    ret = lxml.html.fromstring(urllib2.urlopen(form.action, data).read())
+
+    ret.make_links_absolute(form.action)
+    return ret
+
+
+def do_post_back(form, event_target, event_argument, **kwargs):
+    block = kwargs
+    event_argument = ":".join(event_argument.split("$"))
+    block['__EVENTTARGET'] = event_target
+    block['__EVENTARGUMENT'] = event_argument
+    return post_back(form, **block)
