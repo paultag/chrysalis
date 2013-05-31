c4368cc39697680362460c551af681424cd6561a
Paul Tagliamonte <tag@pault.ag>
2013-05-31 14:50:35 -0400

    moved

--- prototyping/bos/util.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-import lxml.html
-
-
-def lxmlize(url):
-    entry = urllib2.urlopen(url)
-    page = lxml.html.fromstring(entry.read())
-    page.make_links_absolute(url)
-    return page
